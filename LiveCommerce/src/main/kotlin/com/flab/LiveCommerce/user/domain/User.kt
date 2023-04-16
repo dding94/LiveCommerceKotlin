7ae0944635532468b6c8a3916033cba33c2fac13
Jeong Myeonggu <audrn6689@gmail.com>
2023-04-16 23:45:51 +0900

    User 클래스 코틀린 전환

--- LiveCommerce/src/main/kotlin/com/flab/LiveCommerce/user/domain/User.kt
+++ LiveCommerce/src/main/kotlin/com/flab/LiveCommerce/user/domain/User.kt
@@ -1,60 +1,23 @@
-package com.flab.LiveCommerce.user.domain;
+package com.flab.LiveCommerce.user.domain
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
+import com.flab.LiveCommerce.common.auth.Role
+import javax.persistence.*
 
 @Entity
-public class User {
-
+class User(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String email;
-    private String password;
-    private String nickname;
+    var id: Long? = null,
+    var email: String? = null,
+    var password: String? = null,
+    var nickname: String? = null,
+
     @Enumerated(EnumType.STRING)
-    private Role role;
-
-    public User(String email, String password, String nickname) {
-        this.email = email;
-        this.password = password;
-        this.nickname = nickname;
-        this.role = Role.USER;
-    }
-
-    protected User() {
-    }
-
-    public static User create(String email, String password, String nickname) {
-        return new User(email, password, nickname);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+    var role: Role? = Role.USER
+) {
+    companion object {
+        fun create(email: String, password: String, nickname: String): User {
+            return User(email = email, password = password, nickname = nickname)
+        }
     }
 }
